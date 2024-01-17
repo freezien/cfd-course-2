bdb5501a81cac7bb01925c3d1cbc6413a49f1f3d
freezien <zonefm120@gmail.com>
2024-01-17 14:05:21 +0700

    Modified

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,4 @@
-const REGEX = {
+const REGREX = {
   email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
   phone: /(84|0[3|5|7|8|9])+([0-9]{8})\b/,
 };
@@ -8,27 +8,26 @@ const validate = (rules, values) => {
 
   for (const ruleKey in rules) {
     for (const rule of rules[ruleKey]) {
-      // Case: required
+      //   Case: Required
       if (rule.required) {
-        // Check required
+        // check required
         if (!!!values[ruleKey]) {
-          errObj[ruleKey] = rule.message || "Vui lòng nhập thông tin";
+          errObj[ruleKey] = rule.message || "Vui lòng nhập";
           break;
         }
       }
 
-      // Case Regex
+      //   Case: Regrex
       if (rule.regrex && values[ruleKey]) {
         let pattern = "";
-        if (rule.regrex in REGEX) {
-          pattern = REGEX[rule.regrex];
+        if (rule.regrex in REGREX) {
+          pattern = REGREX[rule.regrex];
         } else if (rule.regrex instanceof RegExp) {
           pattern = rule.regrex;
         } else {
           pattern = new RegExp(rule.regrex, "gi");
         }
-
-        // Check Regrex
+        // check regrex
         if (!pattern.test(values[ruleKey])) {
           errObj[ruleKey] = rule.message || "Vui lòng nhập đúng định dạng";
           break;
@@ -36,6 +35,7 @@ const validate = (rules, values) => {
       }
     }
   }
+
   return errObj;
 };
 
